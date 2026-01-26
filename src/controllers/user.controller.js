f54bfe5e306e864ee08730e442227603ff94aa2d
Harsh raj <harshraj9162192367@gmail.com>
2026-01-26 15:11:07 +0530

    Add getWatchHistory endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -382,6 +382,58 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
     .status(200)
     .json(new ApiResponse(200, channel[0], "Channel profile fetched successfully"));
  })
+
+ const getWatchHistory = asyncHandler(async (req, res) => {
+  // Implementation for getting user watch history
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id)
+      }
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    username: 1,
+                    avatar: 1
+                  }
+                },
+                {
+                  $addFields: {
+                    owner:{
+                      $first: "$owner"
+                    }
+                }
+              },
+              ]
+            }
+          }
+        ]
+      }
+     }
+  ]);
+  if (!user?.length === 0) {
+    throw new ApiError(404, "User not found");
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user[0].watchHistory, "Watch history fetched successfully"));
+});
+
 export {
     registerUser,
     loginUser,
@@ -393,4 +445,5 @@ export {
     updateUserAvatar,
     updateCoverImage,
     getUserChannelProfile,
+    getWatchHistory,
     }; 
